b805bde98002db5a9500bfe4aff9c55fd2485452
Julie JAUMARY <jaumary.julie@gmail.com>
2019-02-18 17:52:22 +0100

    creating games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  get 'games/create'
+  get 'games/index'
+  get 'games/show'
+  get 'games/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
