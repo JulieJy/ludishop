59a4742920e4a50416555e7632760c9a7910eab7
Julie JAUMARY <jaumary.julie@gmail.com>
2019-02-18 18:02:17 +0100

    adding routes for gamescontroller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  get 'games/create'
-  get 'games/index'
-  get 'games/show'
-  get 'games/destroy'
-  root to: 'pages#home'
+  resources :games, only [:new, :create, :index, :show, :destroy]
+  root to: 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
