cc4eae87fe33eb2a3e123b9f17095e4425cd4ba0
david habault <david.habault@gmail.com>
2019-02-20 18:38:51 +0100

    controller advancement

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
   # POST /users/:user_id/reviews
   def create
     @user = User.find(params[:user_id])
-    @receiver = User.find(params[:receiver_id])
+    @receiver = User.find(param[:receiver_id])
     @review = Review.new(review_params)
     @review.user = @user
     @review.receiver_id = @receiver
