0b8ca6b5f93ee3c17dc780ab3f8b8c4ab209b5f3
hugo ple <hugo.ple2@gmail.com>
2019-02-21 17:39:42 +0100

    cards_final_game#index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+import $clamp from "clamp-js";
+
+document.querySelectorAll('.card-text p').forEach((elem) => {
+  $clamp(elem, { clamp: 3 });
+});
